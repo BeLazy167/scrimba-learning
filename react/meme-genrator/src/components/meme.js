89d1ee77fe3ce7b069eb7cbcbd86c7358a697d9c
BeLazy167 <66153057+BeLazy167@users.noreply.github.com>
2022-04-04 22:38:14 +0530

    started with meme genrator

--- react/meme-genrator/src/components/meme.js
+++ react/meme-genrator/src/components/meme.js
@@ -7,8 +7,18 @@ export default function Meme() {
         bottomText: "",
         randomImage: "https://i.imgflip.com/30b1gx.jpg",
     };
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setMemeImage((prevMemeImage) => {
+            return {
+                ...prevMemeImage,
+                [name]: value,
+            };
+        });
+    }
     const [allMemeImages, setAllMemeImages] = useState(memedata);
     const [memeImage, setMemeImage] = useState(meme);
+    console.log(memeImage);
     function getMeme() {
         const allmeme = allMemeImages.data.memes;
         const randomMeme = allmeme[Math.floor(Math.random() * allmeme.length)];
@@ -27,16 +37,28 @@ export default function Meme() {
                     type="text"
                     placeholder="Top text"
                     className="form--input"
+                    name="topText"
+                    value={memeImage.topText}
+                    onChange={handleChange}
                 />
                 <input
                     type="text"
                     placeholder="Bottom text"
                     className="form--input"
+                    name="bottomText"
+                    value={memeImage.bottomText}
+                    onChange={handleChange}
                 />
                 <button onClick={getMeme} className="form--button">
                     Get a new meme image 🖼
                 </button>
-                <img src={memeImage.randomImage} alt="meme" />
+                <div className="meme">
+                    <img src={memeImage.randomImage} className="meme--image" />
+                    <h2 className="meme--text top">{memeImage.topText}</h2>
+                    <h2 className="meme--text bottom">
+                        {memeImage.bottomText}
+                    </h2>
+                </div>
             </div>
         </main>
     );
